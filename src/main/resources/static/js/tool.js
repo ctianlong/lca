702fb636f7c8d12115877c7de7340e5da352f5c7
ctianlong <chengtl0131@gmail.com>
2017-11-10 00:29:20 +0800

    11.10

--- src/main/resources/static/js/tool.js
+++ src/main/resources/static/js/tool.js
@@ -14,52 +14,162 @@ $(function(){
 		{id:1,text:"开级配沥青磨耗层(OGFC)",density:2.1},
 		{id:2,text:"沥青玛蹄脂面层(SMA)",density:2.45},
 		{id:3,text:"密级配沥青混合料(AC)",density:2.35},
-		{id:4,text:"其它"}
+		{id:4,text:"其它",density:0}
+	];
+	var cdLayerMaterial=[
+		{id:1,text:"密级配沥青稳定碎石",density:2.3},
+		{id:2,text:"开级配沥青稳定碎石",density:2.1},
+		{id:3,text:"密级配水泥、石灰稳定碎石",density:2.3},
+		{id:4,text:"开级配水泥、石灰稳定碎石",density:2.1},
+		{id:5,text:"水泥、石灰稳定土",density:1.8},
+		{id:6,text:"级配碎石",density:2.1}
 	];
 	
-	
-	
-	$("#roadType").select2({
+	var $roadType=$("#roadType").select2({
 		data:cdRoadType,
 		minimumResultsForSearch:-1,
 		language:"zh-CN"
 	});
-	$("#topLayerAsphaltType").select2({
+	var $topLayerAsphaltType=$("#topLayerAsphaltType").select2({
 		data:cdAsphaltType,
 		minimumResultsForSearch:-1,
 		language:"zh-CN"
 	});
-	$("#asphaltTopLayerMaterial").select2({
+	var $asphaltTopLayerMaterial=$("#asphaltTopLayerMaterial").select2({
 		data:cdAsphaltSurfaceMaterial,
 		minimumResultsForSearch:-1,
 		language:"zh-CN"
 	});
-	$("#middleLayerAsphaltType").select2({
+	var $middleLayerAsphaltType=$("#middleLayerAsphaltType").select2({
 		data:cdAsphaltType,
 		minimumResultsForSearch:-1,
 		language:"zh-CN"
 	});
-	$("#asphaltMiddleLayerMaterial").select2({
+	var $asphaltMiddleLayerMaterial=$("#asphaltMiddleLayerMaterial").select2({
 		data:cdAsphaltSurfaceMaterial,
 		minimumResultsForSearch:-1,
 		language:"zh-CN"
 	});
-	$("#belowLayerAsphaltType").select2({
+	var $belowLayerAsphaltType=$("#belowLayerAsphaltType").select2({
 		data:cdAsphaltType,
 		minimumResultsForSearch:-1,
 		language:"zh-CN"
 	});
-	$("#asphaltBelowLayerMaterial").select2({
+	var $asphaltBelowLayerMaterial=$("#asphaltBelowLayerMaterial").select2({
 		data:cdAsphaltSurfaceMaterial,
 		minimumResultsForSearch:-1,
 		language:"zh-CN"
 	});
-	$("#asphaltBaseLayerMaterial").select2({
-		data:,
+	// 基层
+	var $baseLayerMaterial=$("#baseLayerMaterial").select2({
+		data:cdLayerMaterial,
+		minimumResultsForSearch:-1,
+		language:"zh-CN"
+	});
+	var $baseLayerAsphaltType=$("#baseLayerAsphaltType").select2({
+		data:cdAsphaltType,
+		minimumResultsForSearch:-1,
+		language:"zh-CN"
+	});
+	// 底基层
+	var $bottomBaseLayerMaterial=$("#bottomBaseLayerMaterial").select2({
+		data:cdLayerMaterial,
+		minimumResultsForSearch:-1,
+		language:"zh-CN"
+	});
+	var $bottomBaseLayerAsphaltType=$("#bottomBaseLayerAsphaltType").select2({
+		data:cdAsphaltType,
+		minimumResultsForSearch:-1,
+		language:"zh-CN"
+	});
+	// 垫层
+	var $cushionLayerMaterial=$("#cushionLayerMaterial").select2({
+		data:cdLayerMaterial,
+		minimumResultsForSearch:-1,
+		language:"zh-CN"
+	});
+	var $cushionLayerAsphaltType=$("#cushionLayerAsphaltType").select2({
+		data:cdAsphaltType,
 		minimumResultsForSearch:-1,
 		language:"zh-CN"
 	});
 	
+	$baseLayerMaterial.on("select2:select",function(e){
+		switch (e.params.data.id) {
+		case "1":case "2":
+			$("#baseLayerMaterial-CementLime").hide();
+			$("#baseLayerMaterial-asphalt").show();
+			break;
+		case "3":case "4":case "5":
+			$("#baseLayerMaterial-asphalt").hide();
+			$("#baseLayerMaterial-CementLime").show();
+			break;
+		case "6":
+			$("#baseLayerMaterial-asphalt").hide();
+			$("#baseLayerMaterial-CementLime").hide();
+			break;
+		default:
+			break;
+		}
+	});
+	$bottomBaseLayerMaterial.on("select2:select",function(e){
+		switch (e.params.data.id) {
+		case "1":case "2":
+			$("#bottomBaseLayerMaterial-CementLime").hide();
+			$("#bottomBaseLayerMaterial-asphalt").show();
+			break;
+		case "3":case "4":case "5":
+			$("#bottomBaseLayerMaterial-asphalt").hide();
+			$("#bottomBaseLayerMaterial-CementLime").show();
+			break;
+		case "6":
+			$("#bottomBaseLayerMaterial-asphalt").hide();
+			$("#bottomBaseLayerMaterial-CementLime").hide();
+			break;
+		default:
+			break;
+		}
+	});
+	$cushionLayerMaterial.on("select2:select",function(e){
+		switch (e.params.data.id) {
+		case "1":case "2":
+			$("#cushionLayerMaterial-CementLime").hide();
+			$("#cushionLayerMaterial-asphalt").show();
+			break;
+		case "3":case "4":case "5":
+			$("#cushionLayerMaterial-asphalt").hide();
+			$("#cushionLayerMaterial-CementLime").show();
+			break;
+		case "6":
+			$("#cushionLayerMaterial-asphalt").hide();
+			$("#cushionLayerMaterial-CementLime").hide();
+			break;
+		default:
+			break;
+		}
+	});
+	
+	$roadType.on("select2:select",function(e){
+		switch (e.params.data.id) {
+		case "1":
+			$("#concreteSurface").hide();
+			$("#shopBrickSurface").hide();
+			$("#asphaltSurface").show();
+			break;
+		case "2":
+			$("#asphaltSurface").hide();
+			$("#shopBrickSurface").hide();
+			$("#concreteSurface").show();
+			break;
+		case "3":
+			$("#asphaltSurface").hide();
+			$("#concreteSurface").hide();
+			$("#shopBrickSurface").show();
+			break;
+		default:
+			break;
+		}
+	});
 	
 	var tool={
 		stepGo:function(now,next){
@@ -73,9 +183,22 @@ $(function(){
 	}
 	$("#step-inventory").hide();
 	$("#form-input").submit(function(){
+		var area=$("#roadLength").val()*$("#roadWidth").val();
+		if($roadType.val()==1){
+//			var topWeight=area*$("#asphaltTopLayerThickness").val()*$asphaltTopLayerMaterial.select2("data")[0].density;
+//			var middleWeight=area*$("#asphaltMiddleLayerThickness").val()*$asphaltMiddleLayerMaterial.select2("data")[0].density;
+//			var belowWeight=area*$("#asphaltBelowLayerThickness").val()*$asphaltBelowLayerMaterial.select2("data")[0].density;
+//			var baseWeight=area*$("#baseLayerThickness").val()*$asphaltBaseLayerMaterial.select2("data")[0].density;
+//			var buttonBaseWeight=area*$("#asphaltButtonBaseLayerThickness").val()*$asphaltButtonBaseLayerMaterial.select2("data")[0].density;
+//			var cushionWeight=area*$("#asphaltCushionLayerThickness").val()*$asphaltCushionLayerMaterial.select2("data")[0].density;
+//			topWeight/(1+$("#asphaltTopLayerWhetstoneRatio").val()/100);
+			
+		}
+		
+		
 		tool.stepGo(1,2);
-		$("#step-input").slideUp("slow");
-		$("#step-inventory").slideDown("slow");
+		$("#step-input").hide();
+		$("#step-inventory").show();
 		return false;
 	});
 });
